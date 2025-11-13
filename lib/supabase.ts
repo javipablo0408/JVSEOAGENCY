5bb8074673a4fd967ec2716e870cd76075b04af9
JVSEOAGENCY <info@jvseoagency.es>
2025-11-13 13:17:15 +0100

    Restaurar valores por defecto en lib/supabase.ts tambiÃ©n

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-if (!supabaseUrl) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
-}
-
-if (!supabaseAnonKey) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
-}
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://tonuvghrtfiihwslcpze.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvbnV2Z2hydGZpaWh3c2xjcHplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2OTcyNjcsImV4cCI6MjA3ODI3MzI2N30.ZsiVznlhtMdwC-xVvsksj8F7jh-Cff_yy4R4FVlMorc'
 
 // Cliente para uso en componentes del servidor (sin cookies)
-export const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
